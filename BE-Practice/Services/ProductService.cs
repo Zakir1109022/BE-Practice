d6050de4a117382cd156ed6ad7347a25d98cf06e
zakir1109022 <zakirhasan1109022@gmail.com>
2022-08-31 02:44:14 +0600

    basic crud with repository pattern

---
+++ BE-Practice/Services/ProductService.cs
@@ -0,0 +1,44 @@
+﻿using BE_Practice.Configuration;
+using BE_Practice.Services;
+using BE_Practice.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BE_Practice.Repositories;
+
+namespace BE_Practice.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IRepository<Product> _repository;
+        public ProductService(IRepository<Product> repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+        public async Task<IEnumerable<Product>> GetAllAsync()
+        {
+            return await _repository.GetAll();
+        }
+        public async Task<Product> GetByIdAsync(string id)
+        {
+            return await _repository.GetById(id);
+        }
+        public async Task CreateAsync(Product product)
+        {
+           await _repository.Create(product);
+        }
+        public async Task<bool> UpdateAsync(string id, Product product)
+        {
+           return await _repository.Update(product);
+
+        }
+        public async Task<bool> DeleteAsync(string id)
+        {
+           return await _repository.Delete(id);
+        }
+    }
+}
---
+++ BE-Practice/Startup.cs
@@ -0,0 +1,81 @@
+using BE_Practice.Services;
+using BE_Practice.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+
+namespace BE_Practice
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "BE_Practice", Version = "v1" });
+            });
+
+
+            #region Configuration Dependencies
+
+            services.Configure<MongoDbSettings>(Configuration.GetSection("MongoDbSettings"));
+
+            services.AddSingleton<IMongoDbSettings>(serviceProvider =>
+                serviceProvider.GetRequiredService<IOptions<MongoDbSettings>>().Value);
+
+            #endregion
+
+            #region Project Dependencies
+
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+            services.AddScoped<IProductService, ProductService>();
+
+            #endregion
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BE_Practice v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+        }
+    }
+}
